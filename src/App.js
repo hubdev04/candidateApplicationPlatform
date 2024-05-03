1933375c1c85fa3fe68d2588c8c17bd196f42d63
hubdev04 <76955127+hubdev04@users.noreply.github.com>
2024-05-04 00:57:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import JobsList from './jobComponents/jobList';
 import Filters from './jobComponents/filters';
 import { useState } from 'react';
 function App() {
- 
+ // create filters so that any changes in filtering Text  we can pass them as props and according state mangement and filtering is done by reducers
   const [filters, setFilters] = useState({ location: '' ,jobRole:'',minExp:'',minJdSalary:'',workType:''});
 
   return (
