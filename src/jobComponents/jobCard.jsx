31e44009a80fb2ef2c0d68139327282539781640
hubdev04 <76955127+hubdev04@users.noreply.github.com>
2024-05-04 15:29:35 +0530

    Update jobCard.jsx

--- src/jobComponents/jobCard.jsx
+++ src/jobComponents/jobCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
-import fakedata from '../Utils/fakedata.json';
 import HourglassFullTwoToneIcon from '@mui/icons-material/HourglassFullTwoTone';
 import VerifiedOutlinedIcon from '@mui/icons-material/VerifiedOutlined';
 import JobInfo from './jobInfo';
@@ -12,12 +11,9 @@ import ScrollableComponent from '../shared/description/scrollableComponent';
 const JobCard = ({ job }) => {
     // Calculate the number of days since the job posting, using job salary maximum as a random generator seed.
     const postedday = job.maxJdSalary % 30;
-    const companyNames = fakedata?.companyNames;
-    // Retrieve company name from fake data using modulus operation to simulate variability.
-    const companyName = companyNames[(job.maxJdSalary) % 23].name;
     // Calculate estimated salary range based on available data, providing different formats based on data availability.
     //basically using some basic techniques so that data can be distributed random and specific for posteddays and company
-
+    
     // estimated_salary has some values as null . so depending upon data rendering salary 
     const estimated_salary = job.minJdSalary && job.maxJdSalary 
         ? `${job.minJdSalary}k - ${job.maxJdSalary}k ${job.salaryCurrencyCode}`
@@ -36,7 +32,7 @@ const JobCard = ({ job }) => {
                     <Typography sx={{ fontSize: 13 }}>Posted {postedday} days ago</Typography>
                 </Box>
                 {/* Job information component displaying company name, location, and role. */}
-                <JobInfo companyName={companyName} location={job.location} jobRole={job.jobRole}/>
+                <JobInfo companyName={job.companyName} location={job.location} jobRole={job.jobRole}/>
                 {/* Box for salary information and verified icon. */}
                 <Box sx={{ display: 'flex', flexDirection: 'row', marginBottom: 1 }}>
                     <Typography variant="body1" fontWeight={600} color="#5e615f" sx={{ backgroundColor: '#fff', fontSize: '0.8rem' }}>
